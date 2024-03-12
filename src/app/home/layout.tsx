b5afefa5beb1a5071eca80cda1e190eeb63ba84e
_leeyeung <73725183+LeeYeungYeung@users.noreply.github.com>
2024-03-13 01:29:21 +0800

    update

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en" className="h-full bg-gray-100">
+        <body className="h-full">{children}</body>
+      </html>
+    );
+  }
